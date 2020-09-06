4c041b59d5ff63a43fdd6668a6760b97f69995d8
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-09-06 11:10:11 +0900

    issue #40 を解消

--- command_shell.c
+++ command_shell.c
@@ -6,7 +6,7 @@
 /*   By: yiwasa <yiwasa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/07 09:14:29 by yiwasa            #+#    #+#             */
-/*   Updated: 2020/09/06 10:49:49 by yiwasa           ###   ########.fr       */
+/*   Updated: 2020/09/06 10:56:44 by yiwasa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -175,8 +175,9 @@ int		exec_shell_command(char **args, t_list *e_val, t_list **d_val,char **paths,
 		if (WTERMSIG(status) == SIGINT)
 		{
 			// ft_putendl("kiteru\n");
-			// write (1, "\n", 1);
+			write (1, "\n", 1);
 		}
+		return (128 + WTERMSIG(status));
 	}
 	return (WEXITSTATUS(status));
 }
--- sell_execution.c
+++ sell_execution.c
@@ -6,7 +6,7 @@
 /*   By: yiwasa <yiwasa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/24 07:47:02 by yiwasa            #+#    #+#             */
-/*   Updated: 2020/09/04 15:12:15 by yiwasa           ###   ########.fr       */
+/*   Updated: 2020/09/06 11:09:03 by yiwasa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,28 @@ int		shell_execute(char **args, t_edlist *vals)
 {
 	int pipe_count; //パイプの個数。
 	int	rv; //コマンド実行時の返り値を取得。
+	char *state_val;
+	char *state;
 
 	pipe_count = count_pipe(args);
 	if (pipe_count == 0)
 	{
 		rv = no_pipe(args, vals); //ここの返り値を見て、成功したら１、失敗したら０
-		if (rv == 0)
+		if (rv == 100)//exit の場合100が帰るようになっている。
 		{
 			if (!update_val(&(vals->d_val), "?=0"))
 				return (1);
-		}
-		else if (rv == 1)
-		{
-			if (!update_val(&(vals->d_val), "?=1"))
-				return (1);
-		}
-		else if (rv == 100)//exit の場合100が帰るようになっている。
 			return (0);
+		}
+		else
+		{
+			state_val = ft_itoa(rv);
+			state = ft_strjoin("?=", state_val);
+			if (!update_val(&(vals->d_val), state))
+				return (1);
+			free(state_val);
+			free(state);
+		}
 	}
 	else
 	{
