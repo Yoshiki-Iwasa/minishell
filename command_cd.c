e6ad88c341fcff815672b0ede4b1fed70d7cb25e
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-09-09 11:07:17 +0900

    command_cd.c を整形

--- command_cd.c
+++ command_cd.c
@@ -1,17 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command_cd.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiwasa <yiwasa@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/09 11:05:57 by yiwasa            #+#    #+#             */
+/*   Updated: 2020/09/09 11:06:48 by yiwasa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 int	command_cd(char *arg, t_list **e_val)
 {
-	char path[PATH_MAX + 1];
-	char *home;
-	int	chdir_rv;
+	char	path[PATH_MAX + 1];
+	char	*home;
+	int		chdir_rv;
 
 	errno = 0;
 	path[0] = '\0';
-	if(arg == NULL)
-	{
+	if (arg == NULL)
 		arg = find_value(e_val, "HOME=");
-	}
 	else if (arg[0] == '~')
 	{
 		home = find_value(e_val, "HOME=");
