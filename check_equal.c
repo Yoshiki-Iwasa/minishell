60665995e2b2a61b4a601c92f5184889b89a3be4
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-09-09 11:37:13 +0900

    check_equal.c 整形完了

--- check_equal.c
+++ check_equal.c
@@ -6,77 +6,81 @@
 /*   By: yiwasa <yiwasa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/06 12:15:09 by yiwasa            #+#    #+#             */
-/*   Updated: 2020/09/09 11:23:53 by yiwasa           ###   ########.fr       */
+/*   Updated: 2020/09/09 11:36:42 by yiwasa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		pattern_multi_args(char **args, int *i)
+int			pattern_multi_args(char **args, int *i)
 {
 	if (*i > 0)
 	{
 		if (ft_strcmp(args[*i - 1], "export") != 0)
 		{
 			(*i)++;
-			return (1) ;
+			return (1);
 		}
 		return (0);
 	}
 	else
 	{
 		(*i)++;
-		return (1) ;
+		return (1);
+	}
+}
+
+int			pattern_single_args(char **args, char **new_args, int *i, int *j)
+{
+	if (*i > 0)
+	{
+		if (!ft_strcmp(args[*i - 1], "export"))
+		{
+			new_args[*j] = ft_strdup(args[*i]);
+			(*i)++;
+			(*j)++;
+			return (1);
+		}
+		return (0);
+	}
+	else
+	{
+		new_args[*j] = ft_strdup(args[*i]);
+		(*i)++;
+		(*j)++;
+		return (1);
 	}
 }
 
 /*
- ** TEST= pwd とか TEST = pwd みたいなのに対応
- ** TEST= pwd とか TEST =pwd ならイコールついてるやつを消す。
- ** TEST = pwd なら、"TEST: =: unexpected operator" 吐いて終了。<- これはむしするか、、、
- **
+** TEST= pwd とか TEST = pwd みたいなのに対応
+** TEST= pwd とか TEST =pwd ならイコールついてるやつを消す。
+** TEST = pwd なら、"TEST: =: unexpected operator" 吐いて終了。<- これはむしするか、、、**
 */
+
 char		**check_and_change_equal(char **args)
 {
-	int i;
-	int j;
-	char *arg;
-	char **new_args;
+	int		i;
+	int		j;
+	char	**new_args;
 
 	i = 0;
 	j = 0;
 	new_args = malloc(sizeof(char*) * (count_strs(args) + 1));
 	while (args[i])
 	{
-		arg = args[i];
-		if (count_strs(args) > 1 && arg[ft_strlen(arg) - 1] == '=')//args 複数ある
+		if (count_strs(args) > 1 && args[i][ft_strlen(args[i]) - 1] == '=')
 		{
 			if (pattern_multi_args(args, &i))
 				continue ;
 		}
-		if (args[i + 1] == NULL && ft_strlen(arg) != 1 && (arg[ft_strlen(arg) - 1] == '=' || arg[0] == '='))// args 一個だけ。
+		if (args[i + 1] == NULL && ft_strlen(args[i]) != 1 &&\
+			(args[i][ft_strlen(args[i]) - 1] == '=' || args[i][0] == '='))
 		{
-			if (i > 0)
-			{
-				if (!ft_strcmp(args[i - 1], "export"))
-				{
-					new_args[j] = ft_strdup(args[i]);
-					i++;
-					j++;
-					continue;
-				}
-			}
-			else
-			{
-				new_args[j] = ft_strdup(args[i]);
-				i++;
-				j++;
-				continue;
-			}
+			if (pattern_single_args(args, new_args, &i, &j))
+				continue ;
 		}
-		new_args[j] = ft_strdup(args[i]);
-		i++;
-		j++;
+		new_args[j++] = ft_strdup(args[i++]);
 	}
 	new_args[j] = NULL;
 	return (new_args);
@@ -86,7 +90,7 @@ char		**check_and_change_equal(char **args)
  ** シェル変数に使える文字列かどうかをチェック key に アンダーバー以外の特殊文字が入ってたらOUT
 */
 
-int		check_key_str(char *arg)
+int			check_key_str(char *arg)
 {
 	int i;
 
