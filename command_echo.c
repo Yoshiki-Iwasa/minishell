4665cd06f5dae8682207d69de7fd08d85646b2a7
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-09-09 11:05:34 +0900

    command_echo.c を整形

--- command_echo.c
+++ command_echo.c
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   command_echo.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiwasa <yiwasa@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/09 11:03:05 by yiwasa            #+#    #+#             */
+/*   Updated: 2020/09/09 11:04:18 by yiwasa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 int	command_echo(char **args)
 {
 	int i;
+
 	i = 1;
 	if (!ft_strcmp(args[i], "-n"))
 	{
 		i++;
 		while (args[i] != 0)
 		{
-			ft_putstr_fd(args[i], 1);
-			i++;
+			ft_putstr_fd(args[i++], 1);
 			if (args[i] != NULL)
 				write(1, " ", 1);
 		}
@@ -19,8 +31,7 @@ int	command_echo(char **args)
 	{
 		while (args[i] != 0)
 		{
-			ft_putstr_fd(args[i], 1);
-			i++;
+			ft_putstr_fd(args[i++], 1);
 			if (args[i] != NULL)
 				write(1, " ", 1);
 		}
