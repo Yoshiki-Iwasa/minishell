759a4348376457e6bd135766cff3bf40fbab4871
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-09-10 08:03:47 +0900

    add_paths.c の整形完了

--- add_paths.c
+++ add_paths.c
@@ -6,32 +6,31 @@
 /*   By: yiwasa <yiwasa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/07 12:17:53 by yiwasa            #+#    #+#             */
-/*   Updated: 2020/09/09 22:02:11 by yiwasa           ###   ########.fr       */
+/*   Updated: 2020/09/10 08:03:27 by yiwasa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /*
- ** paths に新しいパスを追加する関数。
+** paths に新しいパスを追加する関数。
 */
 
 char	**add_new_path(char *new_path, char **paths)
 {
-	char **new_paths;
-	int i;
+	char	**new_paths;
+	int		i;
 
 	i = 0;
-	while(paths[i])
+	while (paths[i])
 		i++;
-	new_paths = malloc(sizeof(char *) * (i + 2));// 新しいpath と NULL分。
+	new_paths = malloc(sizeof(char *) * (i + 2));
 	i = 0;
 	while (paths[i])
 	{
 		new_paths[i] = ft_strdup(paths[i]);
 		i++;
 	}
-
 	new_paths[i] = ft_strdup(new_path);
 	i++;
 	new_paths[i] = NULL;
@@ -40,46 +39,59 @@ char	**add_new_path(char *new_path, char **paths)
 }
 
 /*
- ** arg[0]が "." または、 "/" で始まっていたら、paths に相対or 絶対パスを追加。そして、arg[0] をパスなしの実行ファイル名に変える。
+** ルートディレクトリのパスを追加（ルートディレクトリのみのパスにする）関数
 */
 
-char	**add_paths_and_change_arg0(char **argZero, t_edlist *vals)
+int		add_root_path(char **arg_zero, char ***paths, int i)
 {
-	int i;
-	char *execFile_ptr;
+	char *exec_file_ptr;
 	char *new_path;
-	char **paths;
+
+	if ((*arg_zero)[i] == '/')
+	{
+		exec_file_ptr = ft_strdup(&(*arg_zero)[i + 1]);
+		new_path = ft_substr(*arg_zero, 0, i);
+		free(*arg_zero);
+		(*paths) = malloc(sizeof(char*) * 2);
+		(*paths)[0] = ft_strdup(new_path);
+		(*paths)[1] = NULL;
+		*arg_zero = exec_file_ptr;
+		free(new_path);
+		return (0);
+	}
+	return (1);
+}
+
+/*
+** arg[0]が "." または、 "/" で始まっていたら、paths に相対or 絶対パスを追加。
+** そして、arg[0] をパスなしの実行ファイル名に変える。
+*/
+
+char	**add_paths_and_change_arg0(char **arg_zero, t_edlist *vals)
+{
+	int		i;
+	char	**paths;
 
 	paths = get_path(vals->e_val);
 	if (paths[0][0] == '\0')
 		paths = get_path(vals->d_val);
 	i = 0;
-	if (((*argZero)[0] == '.' || (*argZero)[0] == '/' ) && (*argZero)[1] != '\0' && paths[0][0] != '\0')
+	if (((*arg_zero)[0] == '.' || (*arg_zero)[0] == '/') &&\
+		(*arg_zero)[1] != '\0' && paths[0][0] != '\0')
 	{
 		free_all(paths, 0);
-		i = ft_strlen(*argZero);
+		i = ft_strlen(*arg_zero);
 		while (i >= 0)
 		{
-			if ((*argZero)[i] == '/') //パスとして最後のスラッシュを探している。
-			{
-				execFile_ptr = ft_strdup(&(*argZero)[i + 1]);//	実行ファイル名前の部分。
-				new_path = ft_substr(*argZero, 0, i);// スラッシュの一個手前まで取ってくる。
-				free(*argZero);
-				paths = malloc(sizeof(char*) * 2);
-				paths[0] = ft_strdup(new_path);
-				paths[1] = NULL;
-				*argZero = execFile_ptr;// arg0 を実行ファイル名のみに。
-				free(new_path);
+			if (add_root_path(arg_zero, &paths, i) == 0)
 				break ;
-			}
 			i--;
 		}
 		return (paths);
 	}
-	else if (paths[0][0] != '\0' && !(((*argZero)[0] == '.' || (*argZero)[0] == '/' ) && (*argZero)[1] != '\0'))
-	{
+	else if (paths[0][0] != '\0' && !(((*arg_zero)[0] == '.' || \
+		(*arg_zero)[0] == '/') && (*arg_zero)[1] != '\0'))
 		return (add_new_path("/", paths));
-	}
 	else
 		return (paths);
 }
