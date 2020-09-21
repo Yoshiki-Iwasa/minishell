e58f4c7da7b300f69fc4fb422a4bfead086e28db
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-09-21 09:33:18 +0900

    issue #76 修正

--- command_cd.c
+++ command_cd.c
@@ -6,7 +6,7 @@
 /*   By: yiwasa <yiwasa@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/09 11:05:57 by yiwasa            #+#    #+#             */
-/*   Updated: 2020/09/14 09:19:44 by yiwasa           ###   ########.fr       */
+/*   Updated: 2020/09/21 09:30:58 by yiwasa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,34 @@ void	put_error_cd(char *arg)
 	ft_putendl_fd(strerror(errno), 1);
 }
 
+void	update_oldpwd(t_list **e_val)
+{
+	char *pwd;
+	char *oldpwd;
+	char *buf;
+
+	buf = NULL;
+	pwd = getcwd(buf, PATH_MAX);
+	oldpwd = ft_strjoin("OLDPWD=", pwd);
+	update_val(e_val, oldpwd);
+	free(oldpwd);
+	free(pwd);
+}
+
+void	update_pwd(t_list **e_val)
+{
+	char *pwd;
+	char *newpwd;
+	char *buf;
+
+	buf = NULL;
+	pwd = getcwd(buf, PATH_MAX);
+	newpwd = ft_strjoin("PWD=", pwd);
+	update_val(e_val, newpwd);
+	free(newpwd);
+	free(pwd);
+}
+
 int		command_cd(char *arg, t_list **e_val)
 {
 	char	path[PATH_MAX + 1];
@@ -27,6 +55,7 @@ int		command_cd(char *arg, t_list **e_val)
 	int		chdir_rv;
 
 	errno = 0;
+	update_oldpwd(e_val);
 	path[0] = '\0';
 	if (arg == NULL)
 		arg = find_value(e_val, "HOME=");
@@ -38,6 +67,7 @@ int		command_cd(char *arg, t_list **e_val)
 		arg = path;
 	}
 	chdir_rv = chdir(arg);
+	update_pwd(e_val);
 	if (chdir_rv == -1)
 	{
 		put_error_cd(arg);
